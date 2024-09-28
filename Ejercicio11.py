3a72a82cb50aed9c779b4c51d928f5cdb10f9c34
WolverinLando <orlandourbanotrejo@gmail.com>
2024-09-28 14:33:47 -0600

    Algoritmo que calcula la multiplicacion de dos numeros

---
+++ Ejercicio11.py
@@ -0,0 +1,10 @@
+"""Autor:  Orlando Urbano Trejo @Lando
+   Fecha:  13-07-2023
+   Correo: orlandourbanotrejo@gmail.com
+
+Algoritmo que calcula la multiplicacion de dos numeros """
+# Entrada de datos
+numero1 = float(input("Numero 1:"))
+numero2 = float(input("Numero 2:"))
+print("Resultado = {:.2f}".format(numero1 * numero2))
+

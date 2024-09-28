8cdb1449a5e6f312479cbcbf82ce6bda39578338
WolverinLando <orlandourbanotrejo@gmail.com>
2024-09-28 14:35:06 -0600

    Algoritmo que determina el precio del boleto de un viaje

---
+++ Ejercicio13.py
@@ -0,0 +1,10 @@
+"""Autor:  Orlando Urbano Trejo @Lando
+   Fecha:  13-07-2023
+   Correo: orlandourbanotrejo@gmail.com
+
+Algoritmo que determina el precio del boleto de un viaje"""
+
+# Entrada de datos
+precio = float(input("Número de kilómetros a recorrer: "))
+print("El precio de tu boleto es de: ${:.2f}".format(precio * 20.50))
+

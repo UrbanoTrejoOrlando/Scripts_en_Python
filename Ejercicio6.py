5c5aa4b7dafdd7ca71b8b39904f77c0e4be647ba
WolverinLando <orlandourbanotrejo@gmail.com>
2024-09-28 12:44:48 -0600

    Algoritmo para determinar el costo que tendra realizar una llamada telefonica

---
+++ Ejercicio6.py
@@ -0,0 +1,11 @@
+"""Autor:  Orlando Urbano Trejo @Lando
+   Fecha:  13-07-2023
+   Correo: orlandourbanotrejo@gmail.com
+
+Algoritmo para determinar el costo que tendra realizar una llamada telefonica """
+
+# Datos de entrada
+tiempo = int(input("Tiempo de la llamada en minutos: "))
+costo = float(input("Costo por minuto en pesos: "))
+print("Costo de la llamada: $",costo*tiempo)
+

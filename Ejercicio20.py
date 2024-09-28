be34bc0b970de6d7ff10b37d60c46a825213b4d3
WolverinLando <orlandourbanotrejo@gmail.com>
2024-09-28 14:30:40 -0600

    Algoritmo que sume tres numeros

---
+++ Ejercicio20.py
@@ -0,0 +1,13 @@
+"""Autor:  Orlando Urbano Trejo @Lando 
+   Fecha:  13-07-2023
+   Correo: orlandourbanotrejo@gmail.com
+
+Algoritmo que sume tres numeros """
+
+# Entrada de datos
+numero1 = int(input("Número 1: "))
+numero2 = int(input("Número 2: "))
+numero3 = int(input("Número 3: "))
+print("Resultado:", numero1 + numero2 + numero3)
+
+

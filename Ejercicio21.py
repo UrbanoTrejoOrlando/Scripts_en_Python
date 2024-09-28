0b3e63e54c0e9864082a4822509935f20ed31512
WolverinLando <orlandourbanotrejo@gmail.com>
2024-09-28 15:13:14 -0600

    Algoritmo que calcula la edad de una persona en meses.

---
+++ Ejercicio21.py
@@ -0,0 +1,11 @@
+"""Autor:  Orlando Urbano Trejo @Lando
+   Fecha:  13-07-2023
+   Correo: orlandourbanotrejo@gmail.com
+
+Algoritmo que calcula la edad de una persona en meses. """
+
+# Entrada de datos
+edad = int(input("Edad en años: "))
+meses = int(input("Cuantos meses: "))
+print("Edad:", (edad * 12) + meses, "meses")
+

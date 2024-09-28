a3925d0a86399322122e7083f547b94edbbb6565
WolverinLando <orlandourbanotrejo@gmail.com>
2024-09-28 15:14:27 -0600

    Algoritmo para determinar si eres mayor de edad.

---
+++ Ejercicio23.py
@@ -0,0 +1,13 @@
+"""Autor:  Orlando Urbano Trejo @Lando
+   Fecha:  13-07-2023
+   Correo: orlandourbanotrejo@gmail.com
+
+Algoritmo para determinar si eres mayor de edad. """
+# Entrada de datos
+edad = int(input("Edad: "))
+# Condiciones
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+

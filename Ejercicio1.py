cd4bcd8cc8680d4f38fa5bccfccccdc0db3480b9
WolverinLando <orlandourbanotrejo@gmail.com>
2024-09-28 12:31:34 -0600

    Algoritmo que determina a que letra perteneces segun la calificacion obtenida

---
+++ Ejercicio1.py
@@ -0,0 +1,30 @@
+"""Autor:  Orlando Urbano Trejo @Lando 
+   Fecha:  13-07-2023
+   Correo: orlandourbanotrejo@gmail.com
+
+Algoritmo que determina a que letra perteneces segun la calificacion obtenida."""
+
+# Datos de entrada
+calificacion = int(input("Calificacion obtenida (0-10):"))
+# Condiciones
+if(calificacion == 1):
+     print("Tu calificacion es F")
+elif(calificacion == 2):
+    print("Tu calificacion es F")
+elif(calificacion == 3):
+    print("Tu calificacion es F")
+elif(calificacion == 4):
+    print("Tu calificacion es F")
+elif(calificacion == 5):
+    print("Tu calificacion es F")
+elif(calificacion == 6):
+    print("Tu calificacion es D")
+elif(calificacion == 7):
+    print("Tu calificacion es D")
+elif(calificacion == 8):
+    print("Tu calificacion es C")
+elif(calificacion == 9):
+    print("Tu calificacion es B")
+elif(calificacion == 10):
+    print("Tu calificacion es A")
+

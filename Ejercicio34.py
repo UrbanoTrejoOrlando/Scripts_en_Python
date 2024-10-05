0b8cb0bffab0fd1a012bfa0c5baf810b60b210ce
WolverinLando <orlandourbanotrejo@gmail.com>
2024-10-05 12:48:32 -0600

    Suma de números pares

---
+++ Ejercicio34.py
@@ -0,0 +1,11 @@
+"""Autor:  Orlando Urbano Trejo (Lando) 
+   Fecha:  27-07-2023
+   Correo: urbanoorlando79@gmail.com
+
+ Ejercicio: Haz un codigo que haga la suma de los primeros diez numeros pares """
+
+Suma = 0
+for i in range(21):
+    if i % 2 == 0:
+     Suma += i
+print("Suma:",Suma)

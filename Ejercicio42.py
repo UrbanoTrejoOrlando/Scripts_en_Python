69a57759ee79c30b1f81f801c4a339cfadc633f2
WolverinLando <orlandourbanotrejo@gmail.com>
2024-10-05 12:56:43 -0600

    Convertir grados Celsius a Fahrenheit

---
+++ Ejercicio42.py
@@ -0,0 +1,11 @@
+"""Autor:  Orlando Urbano Trejo (Lando) 
+   Fecha:  27-07-2023
+   Correo: urbanoorlando79@gmail.com
+
+ Ejercicio: Convertir de grados Celcius a Fahrenheit """
+
+Conversion = 0
+Grados = float(input("Grados a convertir: "))
+Conversion = (Grados * 1.8) + 32
+print("Conversion: ",Conversion)
+

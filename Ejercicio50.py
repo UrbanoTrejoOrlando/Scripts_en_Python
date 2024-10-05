9a9caa1e605def3cb5f0487c1879e4fe10022637
WolverinLando <orlandourbanotrejo@gmail.com>
2024-10-05 13:05:49 -0600

    Calcular la longitud de una cadena

---
+++ Ejercicio50.py
@@ -0,0 +1,9 @@
+"""Autor:  Orlando Urbano Trejo (Lando)
+   Fecha:  28-07-2023
+   Correo: urbanoorlando79@gmail.com
+
+Ejercicio: Escribir una palabra y ver cuantos mide la cadena """
+
+Palabra = input("Palabra: ")
+Longitud = len(Palabra)
+print("Longitud:", Longitud)

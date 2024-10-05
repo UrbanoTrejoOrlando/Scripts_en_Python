3ce549b8d5709bac6e697fee1d5016c42ed83e47
WolverinLando <orlandourbanotrejo@gmail.com>
2024-10-05 13:50:56 -0600

    Saber el signo zodiacal de una persona

---
+++ Ejercicio57.py
@@ -0,0 +1,14 @@
+"""Autor:  Orlando Urbano Trejo (Lando)
+   Fecha:  28-07-2023
+   Correo: urbanoorlando79@gmail.com
+
+Ejercicio: Ingresa por teclado el nombre y el signo de cualquier persona e imprima el nombre solo si la per   sona es signo Aries, caso contrario imprimir "No es signo Aries" """
+
+Nombre = input("Ingresa tu nombre: ")
+Signo = input("Ingresa tu signo: ")
+if Signo == "Aries":
+    print("Eres signo Aries, tu nombre es:", Nombre)
+else:
+    print("No eres signo Aries")
+
+

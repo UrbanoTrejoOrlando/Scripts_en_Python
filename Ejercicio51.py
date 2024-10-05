57b84f2e81e6a86f6b252670111daa25144e4998
WolverinLando <orlandourbanotrejo@gmail.com>
2024-10-05 13:45:16 -0600

    Convertir una cadena de texto a mayuscula

---
+++ Ejercicio51.py
@@ -0,0 +1,10 @@
+"""Autor:  Orlando Urbano Trejo (Lando)
+   Fecha:  28-07-2023
+   Correo: urbanoorlando79@gmail.com
+
+Ejercicio: Convierte a mayuscula cualquier cadena de texto """
+
+Texto = input("Ingresa una cadena de texto: ")
+Mayuscula = Texto.upper()
+print("Texto en mayúsculas:", Mayuscula)
+
